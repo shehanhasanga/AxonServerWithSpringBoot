4b345811360d62a1dc4164a2c802025da792bece
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-08-02 18:13:20 +0530

    added a new order service

---
+++ OrdersService/src/main/java/com/example/OrdersService/query/OrderEventsHandler.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.OrdersService.query;
+
+import com.example.OrdersService.core.data.OrderEntity;
+import com.example.OrdersService.core.data.OrdersRepository;
+import com.example.OrdersService.core.events.OrderCreatedEvent;
+import org.axonframework.config.ProcessingGroup;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@ProcessingGroup("order-group")
+public class OrderEventsHandler {
+    
+    private final OrdersRepository ordersRepository;
+    
+    public OrderEventsHandler(OrdersRepository ordersRepository) {
+        this.ordersRepository = ordersRepository;
+    }
+
+    @EventHandler
+    public void on(OrderCreatedEvent event) throws Exception {
+        OrderEntity orderEntity = new OrderEntity();
+        BeanUtils.copyProperties(event, orderEntity);
+ 
+        this.ordersRepository.save(orderEntity);
+    }
+    
+}
